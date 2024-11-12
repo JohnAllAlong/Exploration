a24fdbfd65934770830310cd6c4b88cdb5d59271
Conor Daly <conordaly2525@gmail.com>
2024-11-12 10:13:29 -0600

    Changed function names in PlayerMove

--- Assets/PlayerInput/PlayerMove.cs
+++ Assets/PlayerInput/PlayerMove.cs
@@ -7,13 +7,13 @@ public class PlayerMove : MonoBehaviour
 {
     [SerializeField] private float _moveSpeed;
 
-    public void KeyboardMove(Values input)
+    public void VecKeyboardMove(Values input)
     {
         print(input.vector);
         transform.position += (Vector3)input.vector * Time.deltaTime * _moveSpeed;
     }
 
-    public void GamepadMove(Values input)
+    public void VecGamepadMove(Values input)
     {
         print(input.vector);
         transform.position += (Vector3)input.vector * Time.deltaTime * _moveSpeed;
