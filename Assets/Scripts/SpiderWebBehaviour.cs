5aa7b29c5984cf4c4c90ee50b099de804bcfc0e2
John Carlson <johnathonccontact@gmail.com>
2024-11-13 15:37:40 -0600

    SpiderWeb Trap Working
    The web slows the player movement speed down. Ideally I want it to pull the player back in when the player is not pressing a button. Beyond that, you should be able to break the connection to the web either with button mashing or shooting it etc.
    I also want to scale the web in the direction the player is moving away from it so it stretches.

---
+++ Assets/Scripts/SpiderWebBehaviour.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpiderWebBehaviour : MonoBehaviour
+{
+    private GameObject victim;
+    private PlayerMove movement;
+    private bool isTrapped = false;
+    private float pullForce;
+    private float maxDistance = 2.0f;
+    private float distance;
+    public float pullScaler = 0.01f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        distance = Vector3.Distance(transform.position, victim.transform.position);
+        pullForce = Mathf.Max(0, maxDistance - distance);
+
+        if (isTrapped)
+        {
+            movement._moveSpeed = pullForce;
+            if (movement._moveSpeed == 0)
+            {
+                victim.transform.position -= transform.position * pullScaler;
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        victim = collision.gameObject;
+        movement = collision.GetComponent<PlayerMove>();
+        isTrapped = true;
+    }
+}
