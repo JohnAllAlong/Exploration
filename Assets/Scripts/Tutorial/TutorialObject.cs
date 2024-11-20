52005ec230938249e4e2296db06dbed896d461bd
Conor Daly <conordaly2525@gmail.com>
2024-11-20 14:37:47 -0600

    Started implementing tutorial

---
+++ Assets/Scripts/Tutorial/TutorialObject.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TutorialObject : MonoBehaviour
+{
+    public UnityEvent triggerEnter = new UnityEvent();
+    [SerializeField] private string _tagToCheckFor;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("yeah");
+        if(collision.gameObject.tag == _tagToCheckFor)
+        {           
+            // in this event, pass in the number of the tutorial phase
+            // you want to activate
+            triggerEnter.Invoke();
+            gameObject.SetActive(false);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == _tagToCheckFor)
+        {
+            // in this event, pass in the number of the tutorial phase
+            // you want to activate
+            triggerEnter.Invoke();
+            gameObject.SetActive(false);
+        }
+    }
+}
