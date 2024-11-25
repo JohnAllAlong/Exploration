1d3d07d29888155b815948b596ec953076df2b66
Ken Jimeno <kbjimeno@gmail.com>
2024-11-25 12:31:05 -0600

    The Mantis is updated through FixedUpdate
    Since Cinemachine is called on FixedUpdate, I had to call the Mantis' movement on FixedUpdate instead of Update, to prevent jittery movement

--- Assets/Scripts/Boss/BossMovement.cs
+++ Assets/Scripts/Boss/BossMovement.cs
@@ -3,23 +3,36 @@ using UnityEngine;
 public class BossMovement : MonoBehaviour
 {
     [SerializeField] private float movementSpeed;
-
     [SerializeField] private float targetRangeY;
-    private Transform playerPos;
 
+    //Cache the player's Transform component
+    private Transform playerPos;
     
     private void OnEnable() {
+        //Find the player on the scene, and get its Transform component
         playerPos = FindObjectOfType<PlayerMove>().GetComponent<Transform>();
     }
 
-    private void Update() {
+    private void FixedUpdate() {
+        //If the player is in the scene
         if (playerPos) {
+            //If the player is on the above the Mantis, set the directionY to 1. Else if they are below, set to -1;
             int directionY = transform.position.y < playerPos.position.y ? 1 : -1;
+
+            //If the player is on the left side of the Mantis, set the directionX to -1. Else, set to 1;
             int directionX = transform.position.x > playerPos.position.x ? -1 : 1;
 
-            if (transform.position.y >= playerPos.position.y - targetRangeY && transform.position.y <= playerPos.position.y + targetRangeY) {
+            Vector2 distanceFromPlayer = playerPos.position - transform.position;
+
+            //If the player is further away from the Mantis on the X-axis
+            if (Mathf.Abs(distanceFromPlayer.x) > Mathf.Abs(distanceFromPlayer.y)) {
+
+                //Horizontally move the Mantis toward the player
                 transform.Translate(new Vector2(directionX, 0) * movementSpeed * Time.deltaTime);
-            } else {
+            }
+            else 
+            {
+                //Vertically move the Mantis toward the player
                 transform.Translate(new Vector2(0, directionY) * movementSpeed * Time.deltaTime);
             }
         }
