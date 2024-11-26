b14d5a40adc1bbfbdf79d85a0d1a2955332c8aa4
Sapin5 <123406040+Sapin5@users.noreply.github.com>
2024-11-25 18:00:59 -0600

    Added HP
    HP class added for enemies
    Can take Damage now
    Added some more logic to chase to make it more fair to player

--- Assets/Scripts/Enemy/EnemyFast/EnemyFast.cs
+++ Assets/Scripts/Enemy/EnemyFast/EnemyFast.cs
@@ -3,13 +3,14 @@ using UnityEngine;
 public class EnemyFast : EnemyWander
 {
     private EnemyChase enemyChaseScript;
+    private EnemyHP EHP;
 
     [Header("Enemy Information")]
     public const float ENEMY_ID = 02;
     public bool isEnemyAlive = true;
 
     [Header("Animation Controller")]
-    [SerializeField]private EnemyanimatorController EAC;
+    [SerializeField] private EnemyanimatorController EAC;
 
     protected void Awake(){
         // get enemy transfrom
@@ -17,6 +18,8 @@ public class EnemyFast : EnemyWander
 
         // Sets chase script if not done so in editor
         if(enemyChaseScript ==null) enemyChaseScript = GetComponent<EnemyChase>();
+
+        if(EHP ==null) EHP = GetComponent<EnemyHP>();
         
         // Sets EAC. Needs to be specified since each enemy has two animators
         if(EAC == null) EAC = GetComponent<Transform>().GetChild(0).GetComponentInChildren<EnemyanimatorController>();
@@ -29,15 +32,25 @@ public class EnemyFast : EnemyWander
 
     // Runs once per frame
     protected void Update(){
+        if(EHP.GetHP() > 0){
+            Act();
+        }else{
+            isEnemyAlive = false;
+            EAC.PlayState(100);
+        }
+    }
+
+    protected void Act(){
         // Switches between chase state and wander state
         if(!enemyChaseScript.GetChaseState()){
+            Debug.Log("Enemy wander");
             // Moves player, and alse sets the current animation state
             EAC.PlayState(Move());
 
             // Runs player detection to see if player is in range
             enemyChaseScript.PlayerDetected();
         }else{
-
+            Debug.Log("enemy Chase");
             // Runs chase script
             EAC.PlayState(enemyChaseScript.Chase());
         }
