24093a2529c3da3874b3ab5ce1d67ca5e76c4fbc
Aiden C. Desjarlais <aiden@aidendes.com>
2024-11-28 14:33:18 -0600

    HOLY FUCKING SHIT I FIXED IT THANK GOD

--- Assets/Scripts/TrapdoorTeleport.cs
+++ Assets/Scripts/TrapdoorTeleport.cs
@@ -1,74 +1,68 @@
-using CustomInput.Events;
+using System;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Windows;
 
 public class TrapdoorTeleport : MonoBehaviour
 {
     [SerializeField]
     private GameObject targetDoor;
-    [SerializeField]
     private bool canTravel = false;
+    private bool interactWasPressed = false;
+    //private bool startCounting = false;
 
-    private Transform PlayerTransform => PlayerData.singleton.GetPlayerTransform();
-    private Timer PlayerTeleporterCooldown => PlayerData.singleton.GetPlayerTeleporterCooldownTimer();
+    //private float defaultTime = 0.5f;
+    //private float delayTimer;
 
-
-    private void OnEnable()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        InputHandler.singleton.ContBtnOnInteraction += UseTrapdoor;
-    }
-
-    private void OnDisable()
-    {
-        InputHandler.singleton.ContBtnOnInteraction -= UseTrapdoor;
-    }
-
-    
-    private void OnTriggerStay2D(Collider2D collision)
-    {
-        if (collision.CompareTag("Player"))
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            //startCounting = true;
             canTravel = true;
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision) //This should eventually get moved to the player input, under the generic Interact Unity Event
+    {
+        canTravel = true;
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            if (canTravel && interactWasPressed)
+            {
+                collision.gameObject.transform.position = targetDoor.transform.position;
+                //delayTimer = 0;
+                //canTravel = false;
+            }
+        }
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
-        if (collision.CompareTag("Player"))
-            canTravel = false;
+        //startCounting = false;
+        //delayTimer = 0;
+        canTravel = false;
     }
 
-
-    private void UseTrapdoor(Values input)
-    {
-        print(input.pressed + " " + canTravel);
-        if (input.pressed && canTravel)
-        {
-            print(PlayerTeleporterCooldown.GetElapsedTime());
-            if (PlayerTeleporterCooldown.IsRunning()) return;
-            PlayerTeleporterCooldown.StartTimer();
-
-            canTravel = false;
-            PlayerTransform.position = targetDoor.transform.position;
-        }
-
-    }
-    /*
     public void Update()
     {
-        Debug.Log(delayTimer);
-        delayTimer += Time.deltaTime;
-        if (delayTimer >= defaultTime)
+        /*if (startCounting)
+        {
+            delayTimer += Time.deltaTime;
+        }*/
+
+        /*if (delayTimer >= defaultTime)
         {
             canTravel = true;
-        }
+        }*/
 
         if (Input.GetKeyDown("space"))
         {
             interactWasPressed = true;
         }
-        if (Input.GetKeyUp("space"))
+        else
         {
             interactWasPressed = false;
         }
     }
-    */
 }
