490db16653f945cc21bbc514052f91397a5be47d
Ken Jimeno <kbjimeno@gmail.com>
2024-12-04 11:59:30 -0600

    New book can detect the player and destroys itself if not hit after 5s

--- Assets/Scripts/Boss/BookProjectile.cs
+++ Assets/Scripts/Boss/BookProjectile.cs
@@ -6,6 +6,9 @@ public class BookProjectile : MonoBehaviour
 {
     [SerializeField] private float projectileSpeed;
 
+
+    private const float MAX_LIFETIME = 5f;
+
     private Vector2 targetDirection;
 
     public void MoveTowards(Vector2 targetDirection) {
@@ -14,12 +17,13 @@ public class BookProjectile : MonoBehaviour
 
     private void Update() {
         transform.Translate(targetDirection * projectileSpeed * Time.deltaTime);
+
+        Destroy(gameObject, MAX_LIFETIME);
     }
 
     private void OnTriggerEnter2D(Collider2D other) {
         if (other.CompareTag("Player")) {
             //Apply damage to the player
-            Debug.Log("The book hit the Player");
             Destroy(gameObject);
         }
     }
