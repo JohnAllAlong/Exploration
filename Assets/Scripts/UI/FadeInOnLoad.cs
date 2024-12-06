a73b5a677faab769995793cd3a8ba252b93c7d6b
Conor Daly <conordaly2525@gmail.com>
2024-12-06 11:00:22 -0600

    Made significant tutorial progress

--- Assets/Scripts/UI/FadeInOnLoad.cs
+++ Assets/Scripts/UI/FadeInOnLoad.cs
@@ -1,22 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class FadeInOnLoad : MonoBehaviour
 {
     [SerializeField] private SpriteRenderer _fader;
+    [SerializeField] private Image _imageFader;
     [SerializeField] private float _fadeTime;
 
     protected void OnEnable()
     {
-        _fader.enabled = true;
+        if (_imageFader == null)
+        {
+            _fader.enabled = true;
+            _fader.color = new Color(_fader.color.r, _fader.color.g, _fader.color.b, 1);
+        }
+        else
+        {
+            _imageFader.enabled = true;
+            _imageFader.color = new Color(_imageFader.color.r, _imageFader.color.g, _imageFader.color.b, 1);
+        }
     }
 
     protected void Start()
     {
-        GUIUtilitys.FadeOutSprite(_fader, _fadeTime, () =>
-        {
-            _fader.enabled = false;
-        });
+
+        new Timer(1).OnEnd(() => {
+            if (_imageFader == null)
+            {
+                GUIUtilitys.FadeOutSprite(_fader, _fadeTime, () =>
+                {
+                    _fader.enabled = false;
+                });
+            }
+            else
+            {
+                GUIUtilitys.FadeOutSprite(_imageFader, _fadeTime, () =>
+                {
+                    _imageFader.enabled = false;
+                });
+            }
+        }).StartTimer();
     }
 }
